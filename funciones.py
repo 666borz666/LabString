f16e0df79fa55f92e8e25ef238a03d45b2bd55e8
666borz666 <iloveass3333@gmail.com>
2023-09-12 15:18:00 -0600

    Funciones: 1. Decodifique el área y el tipo 2. Decodifique el piso y pasillo 3. Decodifique el detalle

--- funciones.py
+++ funciones.py
@@ -14,7 +14,23 @@ listaPiso=[1, 2, 3, 4, 5, 6, 7, 8, 9]
 listaPasillo=list(range(1,100))
 listaFila= list(range(1,100))
 listaColumna=list(range(1,100))
-code=str("0201-304-i0506")
+listaLado=["i","d"]
+code=str("0201-34-i0506")
+#funcion separar código
+def separarCódigo(code):
+    """
+    F:
+    E:
+    S:
+    """
+    tipo=str(code[1:2])
+    area=str(code[3:4])
+    piso=str(code[5:6])
+    pasillo=str(code[6:8])
+    lado=str(code[9:10])
+    fila=str(code[10:12])
+    columna=str(code[12:14])
+    return tipo, area, piso, pasillo, lado, fila, columna
 
 #Decodificar el área y el tipo
 def decodificarAyT(code):
@@ -43,4 +59,33 @@ def decodificarAyT(code):
             area=str("médica")
     return print("El material bibliográfico es de tipo",tipo,"del área",area)
 
-decodificarAyT(code)
+#Decodificar el tipo y pasillo
+def decodificarPyP(code):
+    """
+    F:
+    E:
+    S:
+    """
+    piso=int(code[5:6])
+    pasillo=int(code[6:8])
+    if piso in listaPiso and pasillo in listaPasillo:
+        return print("El material bibliográfico está en el piso",piso,"y el pasillo",pasillo)
+
+#Decodificar el tipo y pasillo
+def decodificarLyFyC(code):
+    """
+    F:
+    E:
+    S:
+    """
+    lado=str(code[8:9])
+    fila=int(code[9:11])
+    columna=int(code[11:13])
+    if lado in listaLado and fila in listaFila and columna in listaColumna:
+        if lado == "i":
+            lado ="izquierdo"
+        if lado == "d":
+            lado ="derecho"
+    return print("El material bibliográfico está en: \nLado",lado,"\nFila",fila,"\nColumna",columna)
+
+decodificarLyFyC(code)
